e0afb4e24d271bad0871346834f7728111a38631
bassy1992 <wyarquah@gmail.com>
2026-05-21 21:09:11 +0000

    Add storages to INSTALLED_APPS - CRITICAL FIX

--- back/config/settings.py
+++ back/config/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'django_filters',
+    'storages',  # For DigitalOcean Spaces / S3 storage
     
     # Local apps
     'properties',
