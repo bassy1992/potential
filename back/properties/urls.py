2070a8592460a426c7c52bb1643d5b1a2477a085
bassy1992 <wyarquah@gmail.com>
2026-05-24 10:15:16 +0000

    Add seed properties API endpoint

--- back/properties/urls.py
+++ back/properties/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PropertyViewSet
+from .views import PropertyViewSet, seed_properties
 
 router = DefaultRouter()
 router.register(r'properties', PropertyViewSet, basename='property')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('seed-properties/', seed_properties, name='seed-properties'),
 ]
